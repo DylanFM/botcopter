a7201750dae57764a753086529eeca0c81bf764c
Dylan Fogarty-MacDonald <dylan@dylanfm.com>
2013-05-30 14:30:03 -0500

    Basic controls

--- first.js
+++ first.js
@@ -1,16 +1,39 @@
 var five = require("johnny-five"),
-    // or "./lib/johnny-five" when running from the source
-    board = new five.Board();
+    board = new five.Board(),
+    arDrone = require('ar-drone'),
+    drone = arDrone.createClient();
 
 board.on("ready", function() {
 
   var slider = new five.Sensor({
       pin: "I0",
-      freq: 100
+      freq: 250
   });
 
-  slider.scale([0,10]).on("change", function(err, val) {
-    console.log(this.scaled);
+  drone.takeoff(function () {
+
+    console.log("FLYING");
+
+    slider.scale([0,10]).on("change", function(err, val) {
+      console.log();
+
+      if (this.scaled > 5) {
+
+        drone.up(0.5);
+        console.log("UP " + this.scaled);
+
+      } else if (this.scaled <= 5 && this.scaled > 1) {
+
+          drone.down(0.5);
+          console.log("DOWN " + this.scaled);
+
+      } else {
+
+        drone.land();
+        console.log("LAND " + this.scaled);
+
+      }
+    });
   });
 
 });
