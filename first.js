2a17a0fe8767503fed0032308a4826cda8a86878
Dylan Fogarty-MacDonald <dylan@dylanfm.com>
2013-05-30 18:53:18 -0400

    Up down, go to altitude.

--- first.js
+++ first.js
@@ -1,7 +1,123 @@
-var five = require("johnny-five"),
+var _ = require('underscore'),
+    five = require("johnny-five"),
     board = new five.Board(),
     arDrone = require('ar-drone'),
     drone = arDrone.createClient();
+
+drone.config('general:navdata_demo', "FALSE");
+
+var Drone = function(drone) {
+
+  var flying = false,
+      altitude = 0.0,
+      busy = false;;
+
+  drone.on('altitudeChange', function(m) { 
+    //console.log("Current altitude: " + m);
+    altitude = m;//parseFloat(m.toFixed(2), 10);
+  });
+
+  drone.on('flying', function() {
+    flying = true;
+  });
+
+  drone.on('landed', function() {
+    flying = false;
+    busy = false;
+  });
+
+  this.isAtAltitude = function(m) {
+    return Math.abs(m - altitude) < 0.000001;
+  };
+
+  this.stopAtAltitude = function(m) {
+    if (this.isAtAltitude(m)) {
+      this.stop();
+    } else {
+      _.delay(_.bind(function() {
+        this.stopAtAltitude(m);
+      }, this), 100);
+    }
+  };
+
+  this.stop = function() {
+    drone.stop();
+    busy = false;
+  }
+
+  this.up = function() {
+    if (busy === true) {
+      return;
+    }
+
+    if (this.isFlying()) {
+      console.log("UP");
+      drone.up(0.5);
+    } else {
+      this.takeOff();
+    }
+  };
+
+  this.down = function() {
+    if (busy === true) {
+      return;
+    }
+
+    if (this.isFlying()) {
+      console.log("DOWN");
+      drone.down(0.5);
+
+      // Land if low
+      if (altitude < 0.6) {
+        this.land();
+      }
+    }
+  }
+
+  this.takeOff = function() {
+    console.log("TAKEOFF");
+    drone.takeoff(_.bind(function() {
+      flying = true;
+      this.setAltitude(0.5);
+    }, this));
+  }
+
+  this.land = function() {
+    console.log("LAND");
+    drone.land()
+  }
+
+  this.isFlying = function() {
+    return !!flying;
+  }
+
+  this.setAltitude = function(m) {
+    
+    m = parseFloat(m.toFixed(2), 10);
+
+    console.log("setAlt " + m);
+
+    if (m <= 0.3 && this.isFlying()) {
+      this.land();
+      return;
+    }
+
+    if (busy === true) {
+      return;
+    }
+
+    if (m > altitude) {
+      this.up();
+    } else {
+      this.down();
+    }
+    this.stopAtAltitude(m);
+  }
+
+  return this;
+};
+
+var theDrone = new Drone(drone);
 
 board.on("ready", function() {
 
@@ -10,30 +126,13 @@ board.on("ready", function() {
       freq: 250
   });
 
-  drone.takeoff(function () {
+  // 0 to 3 metres
+  var range = [0,3];
 
-    console.log("FLYING");
+  slider.scale(range).on("change", function(err) {
 
-    slider.scale([0,10]).on("change", function(err, val) {
-      console.log();
+    theDrone.setAltitude(this.scaled);
 
-      if (this.scaled > 5) {
-
-        drone.up(0.5);
-        console.log("UP " + this.scaled);
-
-      } else if (this.scaled <= 5 && this.scaled > 1) {
-
-          drone.down(0.5);
-          console.log("DOWN " + this.scaled);
-
-      } else {
-
-        drone.land();
-        console.log("LAND " + this.scaled);
-
-      }
-    });
   });
 
 });
